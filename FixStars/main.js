7e555c3af97954cf404efa1da29bc7bc6f9c8941
spenceruns <spenceruns@gmail.com>
2020-03-18 01:11:07 +0000

    Changed parallax rate

--- FixStars/main.js
+++ FixStars/main.js
@@ -2,5 +2,5 @@ window.addEventListener("scroll", function (e) {
   // Fill logic here
   var scrolled = window.pageYOffset
   const background = document.querySelector("#imgDiv")
-  background.style.backgroundPositionY = - (scrolled * 2) + 'px'
+  background.style.backgroundPositionY = - (scrolled * 1.75) + 'px'
 });
